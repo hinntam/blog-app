e102be43b6d81958492d0b2c14fe277c405deeb8
John <ngoctam.nguyen@edu.sait.ca>
2025-01-08 15:34:15 -0700

    Update page.tsx

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,11 @@
-import { posts } from '@/app/lib/placeholder-data';
+import { getPosts } from "@/app/lib/data";
 import Post from '@/app/ui/components/posts/Post';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+  const posts = await getPosts();
   
-  
-  const post = posts.find((post) => post.id === id);
+  const post = posts?.find((post) => post.id === id);
   if (!post) {
     return <div>Post not found</div>;
   }
@@ -14,6 +14,6 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     <>
    
       <h1>Post</h1>
-      <Post {...post} author={post.user} />
+      <Post id={post.id} title={post.title} content={post.content} date={post.date} author={post.user} />
     </>)
 }
